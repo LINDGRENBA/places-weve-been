191a7948fe2500a4b6c0953629292381295b30a3
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-15 15:06:41 -0700

    Update scripts.js with 3 objects, places1, places2 and places3.
    Co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+///Business Logic
+
+let places1 = {
+  location: "UP Michigan",
+  landmarks: "Lake Superior",
+  timeOfYear:"Summer",
+  notes:"Lots of great places to camp",
+  why:"The U-P is BEAUTIFUL!!!",
+  bestFood: "Bagels from a local food stand"
+}
+let places2 = {
+  location: "Japan",
+  landmarks: "Kiyomizu Temple",
+  timeOfYear:"Spring",
+  notes:"Check out the Cherry Blossoms",
+  why:"Study Japanese",
+  bestFood: "Okonomiyaki"
+}
+let places3 = {
+  location: "Greece",
+  landmarks: "Lesbos Island",
+  timeOfYear:"Summer/Fall",
+  notes:"Increidble views",
+  why:"Greece has a ton of history and beautiful sights",
+  bestFood: "Great Feta Cheese!"
+}
+
+
+
+
+
+//User-Interface Logic
+$(document).ready(function() {
+
+});
+
+
+
+// function printObject(destination) {
+//   let output = '';
+//   for (let i=0; i<places.length; i++) {
+//     output += <h2>places.location</h2> + <p>places.landmarks</p> ;
+//   }
+  
+// }
+
